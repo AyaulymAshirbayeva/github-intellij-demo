5e7acb970dab81defbde757c7154fc62421097b5
ayaulym <251174@astanait.edu.kz>
2026-01-24 10:18:04 +0500

    git init

--- src/healthcare/Main.java
+++ src/healthcare/Main.java
@@ -3,17 +3,13 @@ package healthcare;
 public class Main {
 
     public static void main(String[] args) {
-
-        // ======== Wards ========
         Ward ward1 = new Ward(101, 4);
         Ward ward2 = new Ward(102, 2);
 
-        // ======== Patients ========
         Patient p1 = new Patient("Artem", 11, ward1);
         Patient p2 = new Patient("Svetlana", 71, ward1);
         Patient p3 = new Patient("Talgat", 47, ward2);
 
-        // Добавляем пациентов в БД
         PatientDB.addPatient(p1);
         PatientDB.addPatient(p2);
         PatientDB.addPatient(p3);
@@ -21,17 +17,14 @@ public class Main {
         System.out.println("\n=== PATIENTS ===");
         PatientDB.readPatients();
 
-        // Обновляем возраст пациента
         PatientDB.UpdatePatient("Artem", 12);
         System.out.println("\n=== PATIENTS AFTER UPDATE ===");
         PatientDB.readPatients();
 
-        // Удаляем пациента
         PatientDB.deletePatient("Talgat");
         System.out.println("\n=== PATIENTS AFTER DELETE ===");
         PatientDB.readPatients();
 
-        // ======== Med Professionals ========
         MedProfessional doc1 = new MedProfessional("Adina Sagynbekova", "Therapist", 10);
         MedProfessional doc2 = new MedProfessional("Zhansaya Spash", "Surgeon", 5);
 
@@ -41,17 +34,14 @@ public class Main {
         System.out.println("\n=== MED PROFESSIONALS ===");
         MedProfessionalDB.readMedProfessionals();
 
-        // Обновляем опыт
         MedProfessionalDB.updateMedExperience("Adina Sagynbekova", 12);
         System.out.println("\n=== MED PROFESSIONALS AFTER UPDATE ===");
         MedProfessionalDB.readMedProfessionals();
 
-        // Удаляем врача
         MedProfessionalDB.deleteMedProfessional("Zhansaya Spash");
         System.out.println("\n=== MED PROFESSIONALS AFTER DELETE ===");
         MedProfessionalDB.readMedProfessionals();
 
-        // ======== Hospitals ========
         Hospital h1 = new Hospital("№1 Hospital", 500, 4.5);
         Hospital h2 = new Hospital("Medical Assistance Group", 300, 4.8);
 
